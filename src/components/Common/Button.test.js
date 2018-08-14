5e6d80d66d30fbd0bdfacce8fb9a59363fe6a793
bla211 <brad.louis.alexander@gmail.com>
2018-08-14 12:38:59 -0400

    [tests] tests for button

---
+++ src/components/Common/Button.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Button from './Button';
+
+configure({ adapter: new Adapter() });
+
+describe('Test Button component', () => {
+    test('Button renders passed title', () => {
+        const title = "Next";
+        const wrapper = shallow(<Button title={title}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+    test('Button fires show errors when clicked if error exists', () => {
+        const isError = true;
+        const onError = jest.fn(); 
+        const wrapper = shallow(<Button isError={isError} onError={onError}/>);
+        wrapper.find('button').simulate('click');
+        expect(onError).toHaveBeenCalled();
+    });
+
+    test('Button fires on click and passes data if no error', () => {
+        const isError = false;
+        const onclick = jest.fn();
+        const data = 'payload'; 
+        const wrapper = shallow(<Button isError={isError} onclick={onclick} data={data}/>);
+        wrapper.find('button').simulate('click');
+        expect(onclick).toHaveBeenCalledWith(data);
+    });
+});
