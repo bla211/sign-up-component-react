81ac38b1a0098647fc5b7318b8d06a66460fffee
bla211 <brad.louis.alexander@gmail.com>
2018-08-14 16:15:36 -0400

    [tests] heading tests

---
+++ src/components/Common/Heading.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Heading from './Heading';
+
+configure({ adapter: new Adapter() });
+
+describe('Test Heading component', () => {
+    test('Heading renders properly when passed "headingType === "h1"" and no message', () => {
+        const headingType = 'h1';
+        const message = [];
+        const wrapper = shallow(<Heading headingType={headingType} message={message}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
+
+describe('Test Heading component', () => {
+    test('Heading renders properly when passed "headingType === "h1"" and a one line nessage', () => {
+        const headingType = 'h1';
+        const message = ['message 1'];
+        const wrapper = shallow(<Heading headingType={headingType} message={message}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
+
+describe('Test Heading component', () => {
+    test('Heading renders properly when passed "headingType === "h1"" and a two line nessage', () => {
+        const headingType = 'h1';
+        const message = ['message 1', 'message 2'];
+        const wrapper = shallow(<Heading headingType={headingType} message={message}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
